74c49338aa7ca6d1692682dd1cdb497020a1bd41
Ziri Houamdi <zirihouamdi@air-de-ziri.home>
2024-10-24 10:14:56 +0200

    add model for validation and documation

--- part2/app/api/v1/amenities.py
+++ part2/app/api/v1/amenities.py
@@ -2,9 +2,16 @@
 
 """Amenity endpoints for the HBnB API."""
 
-from flask_restx import Namespace
+from flask_restx import Namespace # type: ignore
 
 api = Namespace('amenities', description='Amenity operations')
 
 # Placeholder for future operations (POST, GET, PUT)
 
+from flask_restx import fields # type: ignore
+
+# Define the amenity model for input validation and documentation
+amenity_model = api.model('Amenity', {
+    'name': fields.String(required=True, description='Name of the amenity'),
+    'description': fields.String(required=False, description='Description of the amenity')
+})
