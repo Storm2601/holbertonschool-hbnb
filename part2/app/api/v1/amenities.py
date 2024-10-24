d81c51ca8947fdc986bd87d778056b24a4cc38dd
Ziri Houamdi <zirihouamdi@air-de-ziri.home>
2024-10-24 09:43:50 +0200

    add namepsace for the api amenities

--- part2/app/api/v1/amenities.py
+++ part2/app/api/v1/amenities.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+
+"""Amenity endpoints for the HBnB API."""
+
+from flask_restx import Namespace
+
+api = Namespace('amenities', description='Amenity operations')
+
+# Placeholder for future operations (POST, GET, PUT)
+
