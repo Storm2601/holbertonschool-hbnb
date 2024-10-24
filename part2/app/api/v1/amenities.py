18ebc8303ae60f0400e05437e6823150cd904f7a
Ziri Houamdi <zirihouamdi@air-de-ziri.home>
2024-10-24 10:41:28 +0200

    add endpoint to  create amenities

--- part2/app/api/v1/amenities.py
+++ part2/app/api/v1/amenities.py
@@ -15,3 +15,29 @@ amenity_model = api.model('Amenity', {
     'name': fields.String(required=True, description='Name of the amenity'),
     'description': fields.String(required=False, description='Description of the amenity')
 })
+
+from flask_restx import Resource # type: ignore
+from app.services.facade import HBnBFacade
+
+facade = HBnBFacade()
+
+@api.route('/')
+class AmenityList(Resource):
+    @api.expect(amenity_model, validate=True)
+    @api.response(201, 'Amenity successfully created')
+    @api.response(400, 'Amenity already exists')
+    def post(self):
+        """Create a new amenity"""
+        amenity_data = api.payload
+
+        # Simulate uniqueness check for amenity name (to be replaced with persistence validation)
+        existing_amenity = facade.get_amenity_by_name(amenity_data['name'])
+        if existing_amenity:
+            return {'error': 'Amenity already exists'}, 400
+
+        new_amenity = facade.create_amenity(amenity_data)
+        return {
+            'id': new_amenity.id,
+            'name': new_amenity.name,
+            'description': new_amenity.description
+        }, 201
