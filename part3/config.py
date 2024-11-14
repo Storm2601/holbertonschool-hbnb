cdefd5bea17517d71e2ede0ed73865407de0eec5
malak33s <melaksarhani1bga@gmail.com>
2024-11-14 15:51:10 +0100

    add confif pour jwt + conif falsk et sqlite

--- part3/config.py
+++ part3/config.py
@@ -8,7 +8,11 @@ import os
 class Config:
     """Base configuration."""
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', 'default_jwt_secret_key')  # secret pour JWT
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = False
+    # Configuration de la base de données, on utilisera SQLite pour le dev
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///app.db')
 
 
 class DevelopmentConfig(Config):
@@ -16,7 +20,16 @@ class DevelopmentConfig(Config):
     DEBUG = True
 
 
+class ProductionConfig(Config):
+    """Production configuration."""
+    # En production, tu pourrais utiliser MySQL ou une autre base de données.
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'mysql://user:password@localhost/hbnb_db')
+    DEBUG = False
+
+
 config = {
     'development': DevelopmentConfig,
+    'production': ProductionConfig,
     'default': DevelopmentConfig
 }
+
