e0499011eda6fe3e79a32fc2480475f34af89291
Storm2601 <kevinguedon2601@gmail.com>
2024-11-14 15:16:06 +0100

    Update the Flask Application Factory to include the configuration object

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,11 +1,23 @@
 from flask import Flask
 from flask_restx import Api
+from flask_bcrypt import Bcrypt
 from app.api.v1.users import api as users_ns
 
-def create_app():
+bcrypt = Bcrypt()
+
+def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
+
+    # Initialize the API with version and description
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
+
+    # Configure the app using the provided config class
+    app.config.from_object(config_class)
+
+    # Initialize bcrypt
+    bcrypt.init_app(app)
 
     # Register the users namespace
     api.add_namespace(users_ns, path='/api/v1/users')
+
     return app
